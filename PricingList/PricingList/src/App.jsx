4a6f08c8a51f449634191d74c70fc1552d0ecb0b
m1tywaflow <dimamarchuk2708@gmail.com>
2025-03-09 22:40:04 +0200

    PricingList

--- PricingList/PricingList/src/App.jsx
+++ PricingList/PricingList/src/App.jsx
@@ -1,11 +1,9 @@
-import Card from './components/Card'
-import './App.css'
-import PricingList from './components/Card';
+import Card from "./components/Card";
+import "./App.css";
+import PricingList from "./components/Card";
 import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
-  return (
-    <PricingList></PricingList>
-  )
+  return <PricingList></PricingList>;
 }
 
 export default App;
